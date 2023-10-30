87faa2ae5728574fc5ab08a21793e600de5522dc
Alex Pizarro <87215633+RazzMatazz5874@users.noreply.github.com>
2023-10-30 17:34:20 -0500

    added some api code

--- src/components/MainApplication.js
+++ src/components/MainApplication.js
@@ -11,47 +11,47 @@ const LibraryContext = createContext();
 //libraryContext.Provider wraps the child components, so they can access the defined values of the values
 
 function MainApplication() {
-  const [libraries, setLibraries] = useState([]);
-  const [selectedLibrary, setSelectedLibrary] = useState(null);
+    const [libraries, setLibraries] = useState([]);
+    const [selectedLibrary, setSelectedLibrary] = useState(null);
 
-  return (
-    <LibraryContext.Provider value={{ libraries, setLibraries, selectedLibrary, setSelectedLibrary }}>
-      <div className="App">
-        <header>LibroVault</header>
-        <div className="content">
-          <LibraryList />
-          {selectedLibrary && <Books />}
-        </div>
-      </div>
-    </LibraryContext.Provider>
-  );
+    return (
+        <LibraryContext.Provider value={{ libraries, setLibraries, selectedLibrary, setSelectedLibrary }}>
+            <div className="App">
+                <header>LibroVault</header>
+                <div className="content">
+                    <LibraryList />
+                    {selectedLibrary && <Books />}
+                </div>
+            </div>
+        </LibraryContext.Provider>
+    );
 }
 
 //This is the LibraryList component, this is the sidebar that has the list of libraries.
 //This component uses 'useContext' to access 'LibraryContext' and be able to use its functions 
 function LibraryList() {
-  const { libraries, setLibraries, setSelectedLibrary } = useContext(LibraryContext);
+    const { libraries, setLibraries, setSelectedLibrary } = useContext(LibraryContext);
 
-  const addLibrary = (name) => {
-    const newLibrary = { name, books: [] };
-    setLibraries([...libraries, newLibrary]);
-  };
+    const addLibrary = (name) => {
+        const newLibrary = { name, books: [] };
+        setLibraries([...libraries, newLibrary]);
+    };
 
-  return (
-    <div className="sidebar">
-      {libraries.map((library, index) => (
-        <button key={index} onClick={() => setSelectedLibrary(library)}>
-          {library.name}
-        </button>
-      ))}
-      <button onClick={() => {
-        const name = prompt("Enter library name");
-        if (name) addLibrary(name);
-      }}>
-        + Add Library
-      </button>
-    </div>
-  );
+    return (
+        <div className="sidebar">
+            {libraries.map((library, index) => (
+                <button key={index} onClick={() => setSelectedLibrary(library)}>
+                    {library.name}
+                </button>
+            ))}
+            <button onClick={() => {
+                const name = prompt("Enter library name");
+                if (name) addLibrary(name);
+            }}>
+                + Add Library
+            </button>
+        </div>
+    );
 }
 
 //This is the Books Component 
@@ -61,182 +61,222 @@ function LibraryList() {
 
 //This also has the algorithms to search books and sort them
 function Books() {
-  const { libraries, setLibraries, selectedLibrary, setSelectedLibrary } = useContext(LibraryContext);
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [translator, setTranslator] = useState('');
-  const [publicationDate, setPublicationDate] = useState('');
-  const [edition, setEdition] = useState('');
-  const [volumeNumber, setVolumeNumber] = useState('');
-  const [genre, setGenre] = useState('');
-  const [subgenre, setSubgenre] = useState('');
-  const [isbn, setIsbn] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-  const [edit, setEdit] = useState(null);
-  const [sortMethod, setSortMethod] = useState
-  ('default');
-  const [bookId, setBookId] = useState(''); 
+    const { libraries, setLibraries, selectedLibrary, setSelectedLibrary } = useContext(LibraryContext);
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [translator, setTranslator] = useState('');
+    const [publicationDate, setPublicationDate] = useState('');
+    const [edition, setEdition] = useState('');
+    const [volumeNumber, setVolumeNumber] = useState('');
+    const [genre, setGenre] = useState('');
+    const [subgenre, setSubgenre] = useState('');
+    const [isbn, setIsbn] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [edit, setEdit] = useState(null);
+    const [sortMethod, setSortMethod] = useState
+        ('default');
+    const [bookId, setBookId] = useState('');
 
 
 
-  const changeLibraryName = (newName) => {
-    const updatedLibraries = libraries.map(lib => {
-      if (lib === selectedLibrary) {
-        lib.name = newName;
-      }
-      return lib;
-    });
-    setLibraries(updatedLibraries);
-  };
-
-  const deleteLibrary = () => {
-    const updatedLibraries = libraries.filter(lib => lib !== selectedLibrary);
-    setLibraries(updatedLibraries);
-    setSelectedLibrary(null);
-  };
-
-  const addBook = (book) => {
-    if (!book.title || !book.author || !book.genre) {
-      alert('Please complete the Title, Author, and Genre fields to add the book.');
-      return;
-    }
-    const updatedLibraries = libraries.map(lib => {
-      if (lib === selectedLibrary) {
-        lib.books.push(book);
-      }
-      return lib;
-    });
-    setLibraries(updatedLibraries);
-    resetFields();
-              
-  };
-
-  const deleteBook = (bookToDelete) => {
-    const updatedLibraries = libraries.map(lib => {
-      if (lib === selectedLibrary) {
-        lib.books = lib.books.filter(book => book !== bookToDelete);
-      }
-      return lib;
-    });
-    setLibraries(updatedLibraries);
-  };
-
-  const editButton = (book) => {
-    setTitle(book.title);
-    setAuthor(book.author);
-    setTranslator(book.translator);
-    setPublicationDate(book.publicationDate);
-    setEdition(book.edition);
-    setVolumeNumber(book.volumeNumber);
-    setGenre(book.genre);
-    setSubgenre(book.subgenre);
-    setIsbn(book.isbn);
-    setEdit(book);
-  };
-
-  const editBook = (editedBook) => {
-    if (!editedBook.title || !editedBook.author || !editedBook.genre) {
-      alert('Please complete the Title, Author, and Genre fields to edit the book.');
-      return;
-    }
-    const updatedLibraries = libraries.map(lib => {
-      if (lib === selectedLibrary) {
-        lib.books = lib.books.map(book => {
-          if (book === edit) {
-            return editedBook;
-          }
-          return book;
+    const changeLibraryName = (newName) => {
+        const updatedLibraries = libraries.map(lib => {
+            if (lib === selectedLibrary) {
+                lib.name = newName;
+            }
+            return lib;
         });
-      }
-      return lib;
-    });
-    setLibraries(updatedLibraries);
-    resetFields();
-  };
+        setLibraries(updatedLibraries);
+    };
 
-  const resetFields = () => {
-    setTitle('');
-    setAuthor('');
-    setPublicationDate('');
-    setGenre('');
-    setTranslator('');
-    setEdition('');
-    setVolumeNumber('');
-    setSubgenre('');
-    setIsbn('');
-    setEdit(null);
-  };
+    const deleteLibrary = () => {
+        const updatedLibraries = libraries.filter(lib => lib !== selectedLibrary);
+        setLibraries(updatedLibraries);
+        setSelectedLibrary(null);
+    };
 
-  let searchedBooks = selectedLibrary.books.filter(book => book.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    const addBook = (book) => {
+        if (!book.title || !book.author || !book.genre) {
+            alert('Please complete the Title, Author, and Genre fields to add the book.');
+            return;
+        }
 
-  if (sortMethod === 'alphabetical') {
-    searchedBooks.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (sortMethod === 'byAuthor') {
-    searchedBooks.sort((a, b) => a.author.localeCompare(b.author));
-  } else if (sortMethod === 'byGenre') {
-    searchedBooks.sort((a, b) => a.genre.localeCompare(b.genre));
-  }
+        //Book Data to send to API 
+        const bookData = {
+            title,
+            author,
+            translator,
+            publicationDate,
+            edition,
+            volumeNumber,
+            genre,
+            subgenre,
+            isbn,
+        };
 
-  return (
-    <div className="books">
-      <h2>{selectedLibrary.name}</h2>
-      <button onClick={() => {
-        const newName = prompt("Edit library name");
-        if (newName) changeLibraryName(newName);
-      }}>
-        Edit Library Name
-      </button>
-      <button onClick={deleteLibrary}>Delete Library</button>
-      <input type="text" placeholder="Search for a book..." onChange={e => setSearchTerm(e.target.value)} />
+        fetch('http://localhost:8080/api/books', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bookData),
+        })
+            .then((response) => response.json())
+            .then((book) => {
+                if (book.id) {
+                    console.log('New book added successfully with ID:', book.id);
+                    // You can update your UI or perform other actions upon successful book addition.
+    
+                    // Store the book ID in your component's state
+                    setBookId(book.id);
+                } else {
+                    console.error('Failed to add book');
+                    // Handle errors and provide user feedback for failed book addition.
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                // Handle network errors or other issues.
+            });
+            
+        const updatedLibraries = libraries.map(lib => {
+            if (lib === selectedLibrary) {
+                lib.books.push(book);
+            }
+            return lib;
+        });
+        setLibraries(updatedLibraries);
+        resetFields();
 
-      <select value={sortMethod} onChange={(e) => setSortMethod(e.target.value)}>
-        <option value="default">Default Order</option>
-        <option value="alphabetical">Sort Alphabetically By Title</option>
-        <option value="byAuthor">Sort Alphabetically By Author</option>
-        <option value="byGenre">Group By Genre</option>
-      </select>
+    };
 
-      {searchedBooks.map((book, index) => (
-        <div key={index}>
-          <h4>{book.title}</h4>
-          <p>{book.author}</p>
-          <p>{book.translator}</p>
-          <p>{book.publicationDate}</p>
-          <p>{book.edition}</p>
-          <p>{book.volumeNumber}</p>
-          <p>{book.genre}</p>
-          <p>{book.subgenre}</p>
-          <p>{book.isbn}</p>
-          <button onClick={() => deleteBook(book)}>Delete book</button>
-          <button onClick={() => editButton(book)}>Edit book</button>
+
+
+
+    const deleteBook = (bookToDelete) => {
+        const updatedLibraries = libraries.map(lib => {
+            if (lib === selectedLibrary) {
+                lib.books = lib.books.filter(book => book !== bookToDelete);
+            }
+            return lib;
+        });
+        setLibraries(updatedLibraries);
+    };
+
+    const editButton = (book) => {
+        setTitle(book.title);
+        setAuthor(book.author);
+        setTranslator(book.translator);
+        setPublicationDate(book.publicationDate);
+        setEdition(book.edition);
+        setVolumeNumber(book.volumeNumber);
+        setGenre(book.genre);
+        setSubgenre(book.subgenre);
+        setIsbn(book.isbn);
+        setEdit(book);
+    };
+
+    const editBook = (editedBook) => {
+        if (!editedBook.title || !editedBook.author || !editedBook.genre) {
+            alert('Please complete the Title, Author, and Genre fields to edit the book.');
+            return;
+        }
+        const updatedLibraries = libraries.map(lib => {
+            if (lib === selectedLibrary) {
+                lib.books = lib.books.map(book => {
+                    if (book === edit) {
+                        return editedBook;
+                    }
+                    return book;
+                });
+            }
+            return lib;
+        });
+        setLibraries(updatedLibraries);
+        resetFields();
+    };
+
+    const resetFields = () => {
+        setTitle('');
+        setAuthor('');
+        setPublicationDate('');
+        setGenre('');
+        setTranslator('');
+        setEdition('');
+        setVolumeNumber('');
+        setSubgenre('');
+        setIsbn('');
+        setEdit(null);
+    };
+
+    let searchedBooks = selectedLibrary.books.filter(book => book.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    if (sortMethod === 'alphabetical') {
+        searchedBooks.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortMethod === 'byAuthor') {
+        searchedBooks.sort((a, b) => a.author.localeCompare(b.author));
+    } else if (sortMethod === 'byGenre') {
+        searchedBooks.sort((a, b) => a.genre.localeCompare(b.genre));
+    }
+
+    return (
+        <div className="books">
+            <h2>{selectedLibrary.name}</h2>
+            <button onClick={() => {
+                const newName = prompt("Edit library name");
+                if (newName) changeLibraryName(newName);
+            }}>
+                Edit Library Name
+            </button>
+            <button onClick={deleteLibrary}>Delete Library</button>
+            <input type="text" placeholder="Search for a book..." onChange={e => setSearchTerm(e.target.value)} />
+
+            <select value={sortMethod} onChange={(e) => setSortMethod(e.target.value)}>
+                <option value="default">Default Order</option>
+                <option value="alphabetical">Sort Alphabetically By Title</option>
+                <option value="byAuthor">Sort Alphabetically By Author</option>
+                <option value="byGenre">Group By Genre</option>
+            </select>
+
+            {searchedBooks.map((book, index) => (
+                <div key={index}>
+                    <h4>{book.title}</h4>
+                    <p>{book.author}</p>
+                    <p>{book.translator}</p>
+                    <p>{book.publicationDate}</p>
+                    <p>{book.edition}</p>
+                    <p>{book.volumeNumber}</p>
+                    <p>{book.genre}</p>
+                    <p>{book.subgenre}</p>
+                    <p>{book.isbn}</p>
+                    <button onClick={() => deleteBook(book)}>Delete book</button>
+                    <button onClick={() => editButton(book)}>Edit book</button>
+                </div>
+            ))}
+
+            <div>
+                <input type="text" placeholder="Book Title" value={title} onChange={e => setTitle(e.target.value)} />
+                <input type="text" placeholder="Author" value={author} onChange={e => setAuthor(e.target.value)} />
+                <input type="text" placeholder="Translator" value={translator} onChange={e => setTranslator(e.target.value)} />
+                <input type="text" placeholder="Publication Date" value={publicationDate} onChange={e => setPublicationDate(e.target.value)} />
+                <input type="text" placeholder="Edition" value={edition} onChange={e => setEdition(e.target.value)} />
+                <input type="text" placeholder="Volume Number" value={volumeNumber} onChange={e => setVolumeNumber(e.target.value)} />
+                <input type="text" placeholder="Genre" value={genre} onChange={e => setGenre(e.target.value)} />
+                <input type="text" placeholder="Sub-genre" value={subgenre} onChange={e => setSubgenre(e.target.value)} />
+                <input type="text" placeholder="ISBN" value={isbn} onChange={e => setIsbn(e.target.value)} />
+
+                {edit ? (
+                    <button onClick={() => editBook({ title, author, translator, publicationDate, edition, volumeNumber, genre, subgenre, isbn })}>
+                        Update Book
+                    </button>
+                ) : (
+                    <button onClick={() => addBook({ title, author, translator, publicationDate, edition, volumeNumber, genre, subgenre, isbn })}>
+                        Add Book
+                    </button>
+                )}
+            </div>
         </div>
-      ))}
-
-      <div>
-        <input type="text" placeholder="Book Title" value={title} onChange={e => setTitle(e.target.value)} />
-        <input type="text" placeholder="Author" value={author} onChange={e => setAuthor(e.target.value)} />
-        <input type="text" placeholder="Translator" value={translator} onChange={e => setTranslator(e.target.value)} />
-        <input type="text" placeholder="Publication Date" value={publicationDate} onChange={e => setPublicationDate(e.target.value)} />
-        <input type="text" placeholder="Edition" value={edition} onChange={e => setEdition(e.target.value)} />
-        <input type="text" placeholder="Volume Number" value={volumeNumber} onChange={e => setVolumeNumber(e.target.value)} />
-        <input type="text" placeholder="Genre" value={genre} onChange={e => setGenre(e.target.value)} />
-        <input type="text" placeholder="Sub-genre" value={subgenre} onChange={e => setSubgenre(e.target.value)} />
-        <input type="text" placeholder="ISBN" value={isbn} onChange={e => setIsbn(e.target.value)} />
-
-        {edit ? (
-          <button onClick={() => editBook({ title, author, translator, publicationDate, edition, volumeNumber, genre, subgenre, isbn })}>
-            Update Book
-          </button>
-        ) : (
-          <button onClick={() => addBook({ title, author, translator, publicationDate, edition, volumeNumber, genre, subgenre, isbn })}>
-            Add Book
-          </button>
-        )}
-      </div>
-    </div>
-  );
+    );
 }
-
-
 
 export default MainApplication;
